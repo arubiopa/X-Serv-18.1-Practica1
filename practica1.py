a097056d0b31f68065ff59dd96d1b578713522bd
arubiopa <anggylotis@gmail.com>
2016-03-16 22:00:03 +0100

    ejercicio finalizado

--- practica1.py
+++ practica1.py
@@ -19,20 +19,20 @@ class contentApp (webapp.webApp):
 
     # Declare and initialize content
     url1 = {}
-    url2 = {}
+    n_urls = 0
     def formulario(self):
         return ('<form method="POST" action="">formulario urls'
                 + '<input type="text" name="nombre" value=""/>'
                 + '<input type="submit" value="enviar"/>'
-                + '</form>'
+                + '</form>')
 
     def parse(self, request):
         """Return the resource name (including /)"""
-        #tendremos que diferenciar entre put y get
+        #tendremos que diferenciar entre post y get
         metodo = request.split(' ', 2)[0]
         recurso = request.split(' ', 2)[1]
         if metodo == 'POST':
-            cuerpo = request.split('\r\n\r\n ')[1]
+            cuerpo = request.split('\r\n\r\n')[1]
         elif metodo == 'GET':
             cuerpo = ""
 
@@ -41,42 +41,67 @@ class contentApp (webapp.webApp):
     def process(self, peticion):
 
         metodo, recurso, cuerpo = peticion
+        cuerpoencontrado = False
         if metodo == "GET":
             if recurso == '/':
                 httpCode = "200 OK"
-                htmlBody = "<html><body>" + self.formulario()
-                    + "url almacenadas: </br>" + str(self.urls1)
-                    + "</body></html>"
+                htmlBody = ("<html><body>" + self.formulario()
+                    + "url almacenadas: </br>" + str(self.url1))
+                for key in self.url1.keys():
+                    htmlBody += "<p><a href='/" + str(key) +  "'>"+ str(key) + "</a></p>"
+
+                htmlBody += '</body></html>'
                 return (httpCode, htmlBody)
             else:
                 try:
-                    url = self.url2[int(peticion[1][1:])]
-                    httpCode = "300 Redirect"
-                    htmlBody = "<html><head><meta http-equiv='refresh' content='0;url="+ url + "'></head></html>"
+                    url = self.url1[int(peticion[1].split("/")[1])]
+                    httpCode = "307 Redirect"
+                    htmlBody = '<html><body>Redirect....<meta http-equiv="refresh" content="2;url='+ str(url) +'"></body></html>'
                     return (httpCode, htmlBody)
+
                 except KeyError:
                     httpCode = "404 Not Found"
-                    htmlBody = "<html><body>" + self.formulario()
+                    htmlBody = ("<html><body>" + self.formulario()
                         +'<p>Recurso no encontrado</p>'
-                        +'</body></html>'
+                        +'</body></html>')
                     return (httpCode, htmlBody)
+
                 except IndexError:
                     httpCode = "404 Not Found"
-                    htmlBody = "<html><body>" + self.formulario()
+                    htmlBody = ("<html><body>" + self.formulario()
                         +'<p>Recurso no encontrado</p>'
-                        +'</body></html>'
+                        +'</body></html>')
                     return (httpCode, htmlBody)
         elif metodo == "POST":
-            self.content[recurso] = cuerpo
-            httpCode = "200 OK"
-            htmlBody = "Todo ha ido bien"
+            url = cuerpo.split("=")[1]
+
+            if url.find("http%3A%2F%2F") >=  0:
+                url = url.split('http%3A%2F%2F')[1]
+
+            url = "http://" + url
+
+            for key in self.url1.keys():
+                if(self.url1[key] == url):
+                    cuerpoencontrado = True
+                    break
+
+            if not cuerpoencontrado:
+                self.url1[self.n_urls] = url
+                self.n_urls += 1
+
+
+
+            httpCode = "307 Redirect"
+            htmlBody = '<html><body>Redirect....<meta http-equiv="refresh" content="2;url=/"></body></html>'
+            return (httpCode, htmlBody)
         else:
             httpCode = "404 Not Found"
-            htmlBody = "<html><body>" + self.formulario()
-                +'<p>Metodo desconocido</p>'
-                +'</body></html>'
-            return (httpCode, htmlBody)
+            htmlBody = ("<html><body>" + self.formulario()
+                +'<p>Metodo desconocid</p>'
+                +'</body></html>')
+
+        return (httpCode, htmlBody)
 
 
 if __name__ == "__main__":
-    testWebApp = contentApp("localhost", 1235)
+    testWebApp = contentApp("localhost", 1234)
